5981297b86e5f9edd327e11d95e02d7b913f756e
Satender Rathore <satenderjpr@gmail.com>
2026-08-21 00:24:16 +0530

    Update React learning app and add package setup

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+
 // const element = React.createElement('h1', {id: 'heading'}, "Hello World from React!");
-const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
 
 
 /**
@@ -15,16 +18,28 @@ const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
  * </div>
  */
 
-const element = React.createElement('div', { id: 'parent', a: 'b', c: 'd', raja: 'Raja' },
-    [React.createElement('div', { id: 'child' },
-        [React.createElement('h1', {}, 'This is a h1 tag'), React.createElement('h2', {}, 'This is a h2 Tag')]
-    ),
-    React.createElement('div', { id: 'child2' },
-        [React.createElement('h1', {}, 'This is a h1 tag'), React.createElement('h2', {}, 'This is a h2 tag')]
-    )
-    ]
-)
+// const element = React.createElement('div', { id: 'parent', a: 'b', c: 'd', raja: 'Raja' },
+//     [React.createElement('div', { id: 'child' },
+//         [React.createElement('h1', {}, 'This is a h1 tag'), React.createElement('h2', {}, 'This is a h2 Tag')]
+//     ),
+//     React.createElement('div', { id: 'child2' },
+//         [React.createElement('h1', {}, 'This is a h1 tag'), React.createElement('h2', {}, 'This is a h2 tag')]
+//     )
+//     ]
+// )
 
-console.log('Element: ', element);
+const jsxElement = <h1>Hello From JSX</h1>
 
-reactRoot.render(element);
+const ReactElement = () => <div>Hello React Element!</div>
+
+const ReactComponent = () => <div>
+        <ReactElement />
+        <div>Hello React Component!</div>
+    </div>
+  
+const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// console.log('Element: ', element);   
+
+reactRoot.render(<ReactComponent />);
